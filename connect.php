994f708f95f722808c1b3e9939c38dc7ea970f38
Roman Gelman <roman.gelman@icloud.com>
2022-11-06 12:51:35 -0500

    Test

--- connect.php
+++ connect.php
@@ -5,18 +5,12 @@ $dbuser = 'root';
 $dbpass = '';
 $dbname = 'duality_data';
 
-$first_name= $_POST["first_name"];
-$last_name= $_POST["last_name"];
-$username = $_POST["username"];
-$dob = $_POST["dob"]; // FORMAT YYYY-MM-DD
-$pwd = $_POST["pwd"];
-
 // Create database connection
-  $dblink = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 // Check connection was successful
-  if ($dblink->connect_error) {
-     die("Connection failed: " . $dblink->connect_error);
-  }
-  echo "Connected successfully";
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+echo "Connected successfully";
 ?>
